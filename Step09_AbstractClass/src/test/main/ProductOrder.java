02df5ad897d4b864d89dcc39699f95fbb59ac182
Jinse; <jinse1209@naver.com>
2024-12-20 09:42:11 +0900

    Step10 프로젝트 추가함

---
+++ Step09_AbstractClass/src/test/main/ProductOrder.java
@@ -0,0 +1,49 @@
+package test.main;
+
+public class ProductOrder {
+	String productname;
+	int price;
+	int quantity;
+	
+	public static void main(String[] args) {
+		ProductOrder[] products=new ProductOrder[3];
+		
+		ProductOrder pd1= new ProductOrder();
+		pd1.productname= "두부";
+		pd1.price=2000;
+		pd1.quantity=2;
+		products[0]=pd1;
+		
+		ProductOrder pd2= new ProductOrder();
+		pd2.productname= "김치";
+		pd2.price=5000;
+		pd2.quantity=1;
+		products[1]=pd2;
+		
+		ProductOrder pd3= new ProductOrder();
+		pd3.productname= "콜라";
+		pd3.price=1500;
+		pd3.quantity=2;
+		products[2]=pd3;
+		
+		
+		 
+		
+		
+		
+		for(ProductOrder ppp: products) {
+			 
+			System.out.println("상품명은 " + ppp.productname + "이고요오옹  가격은요 " +  ppp.price + "이며  수량은 "  +  ppp.quantity );
+			
+		}
+		
+		
+		
+		
+		
+				
+		
+		
+	}
+
+}
---
+++ Step10_Interface/src/test/mypac/MyRemocon.java
@@ -0,0 +1,17 @@
+package test.mypac;
+
+public class MyRemocon implements Remocon {
+
+	@Override
+	public void up() {
+		System.out.println("볼륨을 올려요!");
+		
+	}
+
+	@Override
+	public void down() {
+		System.out.println("볼륨을 내려요!");
+		
+	} //인터페이스는 implements 해야 한다.
+
+}
